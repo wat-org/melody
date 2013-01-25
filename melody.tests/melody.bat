939670b17d3682b0f6aa2ae5ffdb762e819f2797
guillaume cornet <cornet.guillaume.pierre@gmail.com>
2013-01-25 17:46:57 +0100

    Remove win_melody.properties : because melody.properties can also work on windwos + correct a bug in melody.sh + correct a bug in FS.java

--- melody.tests/melody.bat
+++ melody.tests/melody.bat
@@ -1,12 +1,12 @@
 @echo off
 
 set homedir=%~p0
-set jarsLibraryPath=%homedir%\\common
-set defaultGlobalConfigurationFilePath="%homedir%\\config\\win_melody.properties"
+set jarsLibraryPath=%homedir%/common
+set defaultGlobalConfigurationFilePath="%homedir%/config/melody.properties"
 set MelodyMainClass=com.wat.melody.cli.Launcher
 setLocal EnableDelayedExpansion
 set classpath=
-for /r %jarsLibraryPath% %%a IN (*.jar) do set classpath=!classpath!;%%a
+for /r "%jarsLibraryPath%" %%a IN (*.jar) do set classpath=!classpath!;"%%a"
 
 java -cp %classpath% %MelodyMainClass% %defaultGlobalConfigurationFilePath% %*
 
