87d794ffc2774e08975d1484d825c01258e40b9d
guillaume cornet <cornet.guillaume.pierre@gmail.com>
2013-02-10 21:04:18 +0100

    Introduce the MelodyInterruptedException, which aim is to simplify the InterruptedException throwing.

---
+++ melody.common/src/main/java/com/wat/melody/common/ex/MelodyInterruptedException.java
@@ -0,0 +1,30 @@
+package com.wat.melody.common.ex;
+
+/**
+ * 
+ * @author Guillaume Cornet
+ * 
+ */
+public class MelodyInterruptedException extends InterruptedException {
+
+	private static final long serialVersionUID = -6989878652124536814L;
+
+	public MelodyInterruptedException() {
+		super();
+	}
+
+	public MelodyInterruptedException(String msg) {
+		super(msg);
+	}
+
+	public MelodyInterruptedException(Throwable cause) {
+		super(null);
+		initCause(cause);
+	}
+
+	public MelodyInterruptedException(String msg, Throwable cause) {
+		super(msg);
+		initCause(cause);
+	}
+
+}
